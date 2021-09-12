2742839e6f5a7ea6c0e23323e83098bedfab426d
Anup Patel <anup2328@gmail.com>
2021-09-12 20:55:15 +0530

    Update Card.jsx

--- Card.jsx
+++ Card.jsx
@@ -1,3 +1,8 @@
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+
+
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
